662859c05afbf70c38c30f9635bce3eeba633e7d
Perrence Muzavazi <7643707+mperrence@users.noreply.github.com>
2022-03-14 18:59:24 +0200

    add message service isolated  unit test

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,33 @@
+import {
+  MessageService
+} from "./message.service"
+
+describe('MessageService', () => {
+  let service: MessageService;
+
+  beforeEach(() => {
+    service = new MessageService();
+  })
+
+  it('should have no message to start', () => {
+    expect(service.messages.length).toBe(0);
+  })
+
+  it('should add a message when add is called', () => {
+    service.add('message');
+
+    expect(service.messages.length).toBe(1)
+  })
+
+  it('should remove all messages when clear is called', () => {
+      service.add('message 1');
+
+      expect(service.messages.length).toBe(1);
+
+      service.clear();
+
+      expect(service.messages.length).toBe(0);
+  })
+
+
+})
