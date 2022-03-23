b41de2b1e546c03673757865a68a639514eaff05
Perrence Muzavazi <7643707+mperrence@users.noreply.github.com>
2022-03-23 22:14:14 +0200

    clean uo heroes spec file

--- src/app/heroes/heros.component.spec.ts
+++ src/app/heroes/heros.component.spec.ts
@@ -14,27 +14,30 @@ describe(('HeroComponent'), () => {
             {id: 3, name: 'SuperDude', strength: 55}
         ];
 
-        mockHeroService = jasmine.createSpyObj(['getHeroes','getHeroNo404', 
+        mockHeroService = jasmine.createSpyObj(['getHeroes','getHeroNo404',  
         'addHero', 'deleteHero', 'searchHeroes']);
 
         component = new HeroesComponent(mockHeroService);
     })
 
-    it('should remove the indicated hero from the heroes list', () => {
-        mockHeroService.deleteHero.and.returnValue(of(true));
-        component.heroes = HEROES;
-
-        component.delete(HEROES[2]);
-
-        expect(component.heroes.length).toBe(2);
-    })
-
-    it('should call deleteHero with the correct hero', () => {
-        mockHeroService.deleteHero.and.returnValue(of(true));
-        component.heroes = HEROES;
-
-        component.delete(HEROES[2]);
-
-        expect(mockHeroService.deleteHero).toHaveBeenCalledOnceWith(HEROES[2]);
+    describe('delete', () => {
+        it('should remove the indicated hero from the heroes list', () => {
+            mockHeroService.deleteHero.and.returnValue(of(true));
+            component.heroes = HEROES;
+    
+            component.delete(HEROES[2]);
+    
+            expect(mockHeroService.deleteHero).toHaveBeenCalled();
+            expect(component.heroes.length).toBe(2);
+        })
+    
+        it('should call deleteHero with the correct hero', () => {
+            mockHeroService.deleteHero.and.returnValue(of(true));
+            component.heroes = HEROES;
+    
+            component.delete(HEROES[2]);
+    
+            expect(mockHeroService.deleteHero).toHaveBeenCalledOnceWith(HEROES[2]);
+        })
     })
 })
